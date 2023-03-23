824bced00580537478f7860dbe979866f606fb61
ismail_erol <dildoken1132465@hotmail.com>
2023-03-23 23:22:08 +0300

    Hiyerarşik desen

--- ChainOfResponsibility/Program.cs
+++ ChainOfResponsibility/Program.cs
@@ -10,6 +10,75 @@ namespace ChainOfResponsibility
     {
         static void Main(string[] args)
         {
+            Manager manager = new Manager();    
+            VicePresident vicePresident = new VicePresident();  
+            President president = new President();
+
+            manager.SetSuccessor(vicePresident);  // yöneticinin üstü başkan yardımcısı 
+            vicePresident.SetSuccessor(president);  // başkan yardımcısının üstü başkan şeklinde bir tanımlama. 
+
+            Expense expense = new Expense() { Detail="Training", Amount=110}; // harcamam tanımlama. 
+            manager.HandlerExpense(expense); // harcamayı manager'a gönderiyoruz. 
+
+            Console.ReadLine();
         }
     }
+
+    class Expense
+    {
+        public string Detail { get; set; }
+        public decimal Amount { get; set; }
+    }
+
+    abstract class ExpenseHandlerBase
+    {
+        protected ExpenseHandlerBase Successor;
+        public abstract void HandlerExpense(Expense expense);
+
+        public void SetSuccessor(ExpenseHandlerBase successor) // üst olarak gönderilecek successor 
+        {
+            Successor = successor;
+        }
+    }
+
+    class Manager : ExpenseHandlerBase
+    {
+        public override void HandlerExpense(Expense expense)
+        {
+           if (expense.Amount <= 100)
+            {
+                Console.WriteLine("Manager handled the expense");
+            }
+           else if (Successor != null) 
+            {
+                Successor.HandlerExpense(expense); 
+            }
+        }
+    }
+    class VicePresident : ExpenseHandlerBase
+    {
+        public override void HandlerExpense(Expense expense)
+        {
+            if (expense.Amount > 100 && expense.Amount <= 1000)
+            {
+                Console.WriteLine("Vice President handled the expense");
+            }
+            else if (Successor != null)
+            {
+                Successor.HandlerExpense(expense);
+            }
+        }
+    }
+
+    class President : ExpenseHandlerBase
+    {
+        public override void HandlerExpense(Expense expense)
+        {
+            if (expense.Amount > 1000)
+            {
+                Console.WriteLine("President handled the expense");
+            }
+        }
+    }
+
 }
